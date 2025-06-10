fc4dda5428f6bc1f63b76aea8e5c10d81fa906f4
Parth Koul <parthkoul99@gmail.com>
2025-06-10 11:44:31 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// void sayHello() {
+//     cout << "Hello";
+// }
+
+// int add(int a, int b) {
+//     int x = a + b;
+//     return x;
+// }
+
+// bool isEven(int x) {
+//     return x%2 == 0;
+// }
+
+// string greet(string name) {
+//     return "Hello, " + name;
+// }
+
+// char getGrade(int marks) {
+//     if (marks >= 90) {
+//         return 'A';
+//     } 
+//     else if (marks >= 80) {
+//         return 'B';
+//     } 
+//     else {
+//         return 'C';
+//     }
+// }
+
+// float average(float a, float b) {
+//     return (a+b)/2.0;
+// }
+
+int main() {
+
+    // sayHello();
+    // cout << endl << add(1,5);
+    // cout << endl << isEven(7);
+    // cout << endl << greet("Parth");
+    // cout << endl << getGrade(85);
+    // cout << endl << average(5,9);
+
+    // cout << "Hello," << endl << "I am learning DSA"; //next line
+
+    // int a = 10; //integer
+    // float b = 3.14; //4 byte, double is 8 byte
+    // char c = 'A'; // ' ' not " "
+    // bool flag = true;
+    // string s = "xy z"; // " " used
+    // cout << endl << s << endl << a << " " << b << " "<< c ;
+    
+    // int x;
+    // cin >> x;
+    // cout << "The value of 10x is: " << 10*x;
+    
+    // string name;
+    // cin >> name;
+    // cout << "My name is " << name; //whitespace not taken
+
+    // getline(cin,name); //whitespace is taken
+    // cout << name;
+    
+    // int x;
+    // cin >> x;
+    // if (x > 0) {
+    //     cout << "positive";
+    // } 
+    // else if (x == 0) {   //else if is more efficient
+    //     cout << "zero";
+    // }
+    // else {
+    //     cout << "negative";
+    // }
+
+    // for (int i = 1; i<5; i++) {
+    //     cout << i << " ";
+    // }
+
+    // int i = 1;
+    // while (i<=5) {
+    //     cout << i << " ";
+    //     i=i+3;
+    // }
+
+    // int i = 2;
+    // do {
+    //     cout << "space";
+    //     i=i+1;
+    // } while (i<=1);
+
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    // arr[3] += 2;
+    // cout << arr[3];
+
+    // int arr[5] = {1,2,3,4,5};
+    // cout << arr[0] << endl;
+
+    return 0;
+
+}
+
